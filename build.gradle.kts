7b87bd02b6a6ac2313a51245670ea29066b74181
haresh02 <haresh.jayant@gmail.com>
2024-05-01 22:52:28 +0800

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.android"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/android/spotifind/SpotifindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.android.spotifind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
